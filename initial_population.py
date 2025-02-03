2d9cc041d7287f8574fdae564b57a80a95c7ba7d
Antonio Jimeno Yepes <antonio.jimeno@gmail.com>
2025-02-02 20:36:06 -0800

    Linting

--- initial_population.py
+++ initial_population.py
@@ -1,7 +1,6 @@
 from glob import glob
 from os.path import exists
 
-
 import gp_algorithm
 from deap import base, creator, gp, tools
 from runner import Runner, set_pset
@@ -10,24 +9,19 @@ from runner import Runner, set_pset
 def get_population(population_size):
     pset = set_pset(Runner(), is_arc=False)
 
-    creator.create(
-        "FitnessMin", base.Fitness, weights=(1.0, 0.000000000000005)
-    )
-    creator.create(
-        "Individual", gp_algorithm.PrimitiveTree, fitness=creator.FitnessMin
-    )
+    creator.create("FitnessMin", base.Fitness, weights=(1.0, 0.000000000000005))
+    creator.create("Individual", gp_algorithm.PrimitiveTree, fitness=creator.FitnessMin)
 
     toolbox = base.Toolbox()
 
     toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=4)
-    toolbox.register(
-        "individual", tools.initIterate, creator.Individual, toolbox.expr
-    )
+    toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
     pop = toolbox.population(population_size)
 
     return [str(p) for p in pop]
+
 
 if __name__ == "__main__":
     print(get_population(100))
